ca773239d94ad68fd7136e732031702f74cabfbb
Wim van Gool <vangool.w@gmail.com>
2013-07-15 09:21:59 +0200

    Renamed 'Core' to more descriptive and precise name 'UnitOfWork'.

--- MessageProcessor.Core.Tests/Properties/AssemblyInfo.project.cs
+++ MessageProcessor.Core.Tests/Properties/AssemblyInfo.project.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: Guid("E71C9902-7E79-4EDF-9292-8D1DA81FD07B")]
-[assembly: AssemblyTitle("YellowFlare.MessageProcessor.Tests")]
-[assembly: AssemblyDescription("Contains all unit tests for the MessageProcessing core library.")]
+[assembly: AssemblyTitle("YellowFlare.MessageProcessor.UnitOfWork..Tests")]
+[assembly: AssemblyDescription("Contains all unit tests for the UnitOfWork library.")]
--- MessageProcessor.Core/Properties/AssemblyInfo.project.cs
+++ MessageProcessor.Core/Properties/AssemblyInfo.project.cs
@@ -3,10 +3,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("4669F913-79B7-4581-921A-78F32A0946C6")]
-[assembly: AssemblyTitle("YellowFlare.MessageProcessor.Core")]
-[assembly: AssemblyDescription("Represents the core MessageProcessing library.")]
+[assembly: AssemblyTitle("YellowFlare.MessageProcessor.UnitOfWork")]
+[assembly: AssemblyDescription("Contains components to implement a domain model with using the unit of work pattern.")]
 
-[assembly: InternalsVisibleTo("YellowFlare.MessageProcessor.Core.Tests")]
+[assembly: InternalsVisibleTo("YellowFlare.MessageProcessor.UnitOfWork.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, " +
 "PublicKey=002400000480000094000000060200000024000052534" +
 "1310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b" +
