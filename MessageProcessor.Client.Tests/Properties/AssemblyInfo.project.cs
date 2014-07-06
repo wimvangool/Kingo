8886803df62c0cdf2ae542c14f4a5462108fa7bd
Wim van Gool <vangool.w@gmail.com>
2014-07-06 11:23:38 +0200

    Moved UnitOfWork-projects to appropriate folder.

--- MessageProcessor.Client.Tests/Properties/AssemblyInfo.project.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: Guid("3912F1DA-A7D8-431C-B866-1D066E757E48")]
-[assembly: AssemblyTitle("YellowFlare.MessageProcessing.Client.Tests")]
-[assembly: AssemblyDescription("Contains all tests for the client assembly.")]
