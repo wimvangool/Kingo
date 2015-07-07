a42fba5c6d60194d32ce37ca3ebe5dfd70ae5afc
Wim van Gool <vangool.w@gmail.com>
2015-07-07 21:17:56 +0200

    - Shortened assembly and NuGet package names.

--- Framework/Messaging.Client.DataVirtualizationDemo/Properties/AssemblyInfo.project.cs
+++ Framework/Messaging.Client.DataVirtualizationDemo/Properties/AssemblyInfo.project.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("System.ComponentModel.Messaging.Client.DataVirtualizationDemo")]
+[assembly: AssemblyTitle("Syztem.Messaging.Client.DataVirtualizationDemo")]
 [assembly: AssemblyDescription("Contains a user interface that demonstrates the workings of data virtualization.")]
 [assembly: Guid("A79306EC-14E6-488E-83D0-ADAC5DB6287E")]
--- Framework/Messaging.TestEngine/Properties/AssemblyInfo.project.cs
+++ Framework/Messaging.TestEngine/Properties/AssemblyInfo.project.cs
@@ -3,13 +3,13 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("c6102b0a-4d14-4f2b-9ef5-0e645cc438ba")]
-[assembly: AssemblyTitle("System.ComponentModel.TestEngine")]
+[assembly: AssemblyTitle("Syztem.Messaging.TestEngine")]
 [assembly: AssemblyDescription("Represents a test-engine for projects that are built using messaging-patterns.")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyInformationalVersion("2.0.0.0")]
 
-[assembly: InternalsVisibleTo("System.ComponentModel.Messaging.TestEngine.Tests")]
+[assembly: InternalsVisibleTo("Syztem.Messaging.TestEngine.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, " +
 "PublicKey=002400000480000094000000060200000024000052534" +
 "1310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b" +
