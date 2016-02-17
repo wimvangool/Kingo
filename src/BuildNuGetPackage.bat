8bf5b7ec0760f3b6c3699186f2210cfe9c2423bd
Wim van Gool <vangool.w@gmail.com>
2016-02-17 20:37:44 +0100

    Fixed the post-build events.

--- src/BuildNuGetPackage.bat
+++ src/BuildNuGetPackage.bat
@@ -2,7 +2,11 @@
 
 set ProjectFile=%~1
 set ProjectDir=%~dp1
+set OutputDir=%~2 
 
+if not exist "%OutputDir%" (
+  mkdir "%OutputDir%"
+)
 for %%f in (%ProjectDir%\Properties\*.nuspec) do (
-  nuget pack "%projectfile%" -prop configuration=release -includereferencedprojects -outputdirectory "d:\development\nuget packages"	
+  nuget pack "%ProjectFile%" -prop configuration=release -includereferencedprojects -outputdirectory "%OutputDir% "	
 )
--- src/Kingo/Messaging/Domain/KeyAttribute.cs
+++ src/Kingo/Messaging/Domain/KeyAttribute.cs
@@ -3,7 +3,7 @@
 namespace Kingo.Messaging.Domain
 {
     /// <summary>
-    /// Can be used to decorate mark a field or property as the <see cref="IHasKeyAndVersion{T, K}.Key">Key</see> of an aggregate.
+    /// Can be used to decorate mark a field or property as the <see cref="IHasKey{T}.Key">Key</see> of an aggregate.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public sealed class KeyAttribute : Attribute { }
