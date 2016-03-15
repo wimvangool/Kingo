29436fb90bf37a5133301a75f3e1a0152ffa9b6b
Wim van Gool <vangool.w@gmail.com>
2016-03-15 15:45:22 +0100

    Added missing files.

---
+++ examples/Chess/Players/PlayerService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Kingo.Samples.Chess.Challenges;
+
+namespace Kingo.Samples.Chess.Players
+{    
+    public sealed class PlayerService : ServiceProcessor, IPlayerService
+    {        
+        #region [====== Write Methods ======]
+
+        /// <inheritdoc />
+        public Task RegisterPlayerAsync(RegisterPlayerCommand command)
+        {
+            return HandleAsync(command);
+        }
+
+        public Task ChallengePlayerAsync(ChallengePlayerCommand command)
+        {
+            return HandleAsync(command);
+        }
+
+        #endregion
+
+        #region [====== Read Methods ======]
+
+        /// <inheritdoc />
+        public Task<GetPlayersResponse> GetPlayersAsync(GetPlayersRequest request)
+        {
+            return ExecuteAsync(PlayersTable.SelectAllAsync, request);
+        }       
+
+        #endregion
+    }
+}
