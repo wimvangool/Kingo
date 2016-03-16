4e3bc3af5169ef731ce447ad879cba2ae60f6acc
Wim van Gool <vangool.w@gmail.com>
2016-03-16 11:35:17 +0100

    Implemented SelfCheck-scenarios.

---
+++ examples/Chess.Application.Tests/Games/MovePiece/Check/A.SelfCheckByBishopScenario.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kingo.Messaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kingo.Samples.Chess.Games.MovePiece.Check
+{
+    [TestClass]
+    public sealed class SelfCheckByBishopScenario : MovePieceScenario
+    {
+        protected override IEnumerable<IMessageSequence> Given()
+        {
+            yield return base.Given().Concatenate();
+            yield return WhitePlayerMove("e2", "e4");
+            yield return BlackPlayerMove("e7", "e5");
+            yield return WhitePlayerMove("f1", "b5");
+        }
+
+        protected override MessageToHandle<MovePieceCommand> When()
+        {
+            return BlackPlayerMove("d7", "d6");
+        }
+
+        [TestMethod]
+        public override async Task ThenAsync()
+        {
+            await ExpectedCommandExecutionException();
+        }
+    }
+}
---
+++ examples/Chess.Application.Tests/Games/MovePiece/Check/A.SelfCheckByKnightScenario.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kingo.Messaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kingo.Samples.Chess.Games.MovePiece.Check
+{
+
+    [TestClass]
+    public sealed class SelfCheckByKnightScenario : MovePieceScenario
+    {
+        protected override IEnumerable<IMessageSequence> Given()
+        {
+            yield return base.Given().Concatenate();
+            yield return WhitePlayerMove("g1", "f3");
+            yield return BlackPlayerMove("d7", "d6");
+            yield return WhitePlayerMove("f3", "h4");
+            yield return BlackPlayerMove("e7", "e5");
+            yield return WhitePlayerMove("h4", "f5");
+        }
+
+        protected override MessageToHandle<MovePieceCommand> When()
+        {
+            return BlackPlayerMove("e8", "e7");
+        }
+
+        [TestMethod]
+        public override async Task ThenAsync()
+        {
+            await ExpectedCommandExecutionException();
+        }
+    }
+}
