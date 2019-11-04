5b2606da070f4c43ab4b1255ce12964a1aba92cc
Wim van Gool <vangool.w@gmail.com>
2019-11-04 07:46:35 +0100

    Added core-projects.

---
+++ src/MicroServices.Core.Tests/Properties/AssemblyInfo.project.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("DB281746-3EF4-4F66-9C0E-548BEBAFA04C")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
---
+++ src/MicroServices.Core/Properties/AssemblyInfo.project.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("1B192F0E-6BFD-4DA7-8543-3F0B2F467F89")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
