79834cee0844e5faf3024f7d87cbea2f7737045b
Wim van Gool <vangool.w@gmail.com>
2019-11-09 12:47:55 +0100

    Updated several nuget packages.

--- samples/mastermind/GameService/Contract.Tests/Games/StartGameCommandTest.cs
+++ samples/mastermind/GameService/Contract.Tests/Games/StartGameCommandTest.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Kingo.Games;
+using Kingo.MicroServices.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kingo.MasterMind.GameService.Games
 {
-    class StartGameCommandTest
+    [TestClass]
+    public sealed class StartGameCommandTest : RequestMessageTest<StartGameCommand>
     {
+        [TestMethod]
+        public void Command_IsNotValid_IfGameIdIsNotSpecified()
+        {
+            AssertThat(command =>
+            {
+                command.GameId = Guid.Empty;
+            }).IsNotValid(1);
+        }
+
+        protected override StartGameCommand CreateRequest()
+        {
+            return new StartGameCommand()
+            {
+                GameId = Guid.NewGuid(),
+                PlayerName = "Wim"
+            };
+        }
     }
 }
